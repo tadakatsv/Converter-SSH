653184e226bb67f65040fe61997dfa3fa20c81c6
izanami <dargberimor@gmail.com>
2025-09-29 04:35:41 +0300

    Add convertation Miles to KM

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,15 @@
 package app;
 
 public class Main {
+    private static final double CONV_K = 1.609;
     public static void main(String[] args) {
         System.out.println("Same converter, but now through SSH");
+        int miles = 16;
+        System.out.println("Miles to km: " + convMilesToKm(miles));
     }
+    private static double convMilesToKm(double miles) {
+        return miles * CONV_K;
+    }
+
+
 }
